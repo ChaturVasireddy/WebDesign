5613d7d2c1e9e3c87003061817ad27902c42f354
ChaturVasireddy <chaturvasireddy@gmail.com>
2024-12-09 00:39:36 +0530

    removed loading screen and added images

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,29 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll("img");
-    console.log("Total images to load:", images.length);
-    let loadedImages = 0;
-
-    function imageLoaded() {
-        loadedImages++;
-        console.log(`Image loaded. Progress: ${loadedImages}/${images.length}`);
-        if (loadedImages === images.length) {
-            console.log("All images loaded, hiding loading screen");
-            document.getElementById("loading-screen").style.display = "none";
-        }
-    }
-
-    images.forEach((img) => {
-        if (img.complete) {
-            console.log("Image already loaded:", img.src);
-            imageLoaded();
-        } else {
-            console.log("Waiting for image to load:", img.src);
-            img.addEventListener("load", imageLoaded);
-            img.addEventListener("error", imageLoaded); // Handle errors to avoid infinite loading
-        }
-    });
-});
-
 const lenis = new Lenis({ autoRaf: true, });
 lenis.on('scroll', (e) => { console.log(e); });
 
@@ -39,7 +13,7 @@ mail.style.left = chatur.offsetLeft + "px";
 window.addEventListener('resize', f => { mail.style.left = chatur.offsetLeft + "px"; });
 
 gsap.defaults({ ease: "power1.inOut" });
-dist = 229;
+dist = 228;
 const mailtl = gsap.timeline({ paused: true })
   .to("#mail", { x: -dist })
   .to("#mail", { color: "#f4f4f4", duration: 0.15 }, "<")
@@ -73,4 +47,3 @@ const c3 = document.getElementById("c3");
 gsap.to("#work", { scrollTrigger: { trigger: "#work", start: "top bottom", end: "bottom top", onUpdate: (self) => { c1; c1.style.top = ((3350 * self.progress) - 1700) + "px"; } } });
 gsap.to("#work", { scrollTrigger: { trigger: "#work", start: "top bottom", end: "bottom top", onUpdate: (self) => { c2; c2.style.top = ((-200 * self.progress) -50) + "px"; } } });
 gsap.to("#work", { scrollTrigger: { trigger: "#work", start: "top bottom", end: "bottom top", onUpdate: (self) => { c3; c3.style.top = ((3100 * self.progress) - 1500) + "px"; } } });
-//change to trigger website update
